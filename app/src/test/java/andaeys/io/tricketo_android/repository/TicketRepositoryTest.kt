86ecbf61d9295e7d55507a5461c63623aba77d97
andaeys <ekayudha.sulistianda@quipper.com>
2024-05-22 19:26:53 +0700

    add test for add ticket repo

--- app/src/test/java/andaeys/io/tricketo_android/repository/TicketRepositoryTest.kt
+++ app/src/test/java/andaeys/io/tricketo_android/repository/TicketRepositoryTest.kt
@@ -8,12 +8,14 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertThrows
 import org.junit.Before
 import org.junit.Test
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mock
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 
@@ -53,6 +55,7 @@ class TicketRepositoryTest {
 
         assertEquals(expectedTicketListSize,result.size)
     }
+
     @Test
     fun `getTicket should return empty list of ticket when success but empty`() = runBlocking {
         val expectedTicketListSize = 0
@@ -78,6 +81,42 @@ class TicketRepositoryTest {
 
         val exception = assertThrows(Exception::class.java){
             runBlocking { repository.getTickets() }
+        }
+
+        assertEquals(expectedError.message, exception.message)
+    }
+
+    @Test
+    fun `addTicket should complete with no error when success`() = runBlocking {
+        val ticket = dummyTicketList(1).first()
+        val mockNewTicketRef = mock(DatabaseReference::class.java)
+        val taskCompletionSource = TaskCompletionSource<Void>()
+        taskCompletionSource.setResult(null)
+        val mockTask: Task<Void> = taskCompletionSource.task
+
+
+        val dummyTicketKey = "unique_ticket"
+        `when`(ticketsRef.push()).thenReturn(mockNewTicketRef)
+        `when`(mockNewTicketRef.key).thenReturn(dummyTicketKey)
+        `when`(ticketsRef.child(dummyTicketKey)).thenReturn(mockNewTicketRef)
+        `when`(mockNewTicketRef.setValue(ticket)).thenReturn(mockTask)
+
+        repository.addTicket(ticket)
+
+        verify(mockNewTicketRef).setValue(ticket)
+        assertNotNull(mockNewTicketRef.key)
+    }
+
+    @Test
+    fun `addTicket should throw error when error occurred`() = runBlocking {
+        val ticket = dummyTicketList(1).first()
+        val expectedError = Exception("Something when wrong")
+
+        `when`(ticketsRef.push())
+            .thenAnswer { throw expectedError }
+
+        val exception = assertThrows(Exception::class.java){
+            runBlocking { repository.addTicket(ticket) }
         }
 
         assertEquals(expectedError.message, exception.message)
