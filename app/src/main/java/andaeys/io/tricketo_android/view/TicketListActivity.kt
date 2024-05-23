5aa3864a1f06dafcc80f9511699b09587023d207
andaeys <ekayudha.sulistianda@quipper.com>
2024-05-23 16:27:11 +0700

    add empty TicketListActivity

---
+++ app/src/main/java/andaeys/io/tricketo_android/view/TicketListActivity.kt
@@ -0,0 +1,45 @@
+package andaeys.io.tricketo_android.view
+
+import andaeys.io.tricketo_android.view.ui.theme.TricketoandroidTheme
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class TicketListActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TricketoandroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting2(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    TricketoandroidTheme {
+        Greeting2("Android")
+    }
+}
