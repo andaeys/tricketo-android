f5f48783428df8cb64f32575f09b8c4d841fec84
andaeys <ekayudha.sulistianda@quipper.com>
2024-05-23 13:24:00 +0700

    Add test unit test for TicketLIstViewModel

--- app/src/test/java/andaeys/io/tricketo_android/viewmodels/TicketLIstViewModelTest.kt
+++ app/src/test/java/andaeys/io/tricketo_android/viewmodels/TicketLIstViewModelTest.kt
@@ -1,11 +1,11 @@
-package andaeys.io.tricketo_android.model.viewmodels
+package andaeys.io.tricketo_android.viewmodels
 
 import andaeys.io.tricketo_android.doamin.GetTicketList
 import andaeys.io.tricketo_android.model.TicketItem
 import andaeys.io.tricketo_android.model.state.TicketListState
 import andaeys.io.tricketo_android.repository.dummyTicketList
-import andaeys.io.tricketo_android.viewmodels.TicketLIstViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.StandardTestDispatcher
@@ -27,6 +27,7 @@ class TicketLIstViewModelTest {
     private lateinit var getTicketList: GetTicketList
     private lateinit var viewModel: TicketLIstViewModel
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Before
     fun setUp() {
         Dispatchers.setMain(StandardTestDispatcher())
@@ -34,6 +35,7 @@ class TicketLIstViewModelTest {
         viewModel = TicketLIstViewModel(getTicketList)
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @After
     fun tearDown(){
         Dispatchers.resetMain()
@@ -46,7 +48,7 @@ class TicketLIstViewModelTest {
         val expectedTicketItemList = ticketEntityList.mapIndexed { index, ticket ->
             TicketItem.fromTicketEntity("$index", ticket)
         }
-        val expectedFlowCount = 4
+        val expectedFlowCount = 2
 
         `when`(getTicketList.execute()).thenReturn(expectedTicketItemList)
 
@@ -64,6 +66,52 @@ class TicketLIstViewModelTest {
             val successState = states[1] as TicketListState.Success
             assertEquals(expectedTicketItemList.size, successState.ticketItemList.size)
             assertTrue(expectedTicketItemList.contains(successState.ticketItemList.first()))
+        }
+    }
+
+    @Test
+    fun `should return flow of state with loading and empty when fetch is success with empty result`() = runTest {
+        val expectedFlowCount = 2
+
+        `when`(getTicketList.execute()).thenReturn(emptyList())
+
+        val states = mutableListOf<TicketListState>()
+        val job = launch {
+            viewModel.ticketListState.toList(states)
+        }
+        viewModel.fetchTicketList()
+        advanceUntilIdle()
+
+        assertEquals(expectedFlowCount, states.size)
+        assertTrue(states[0] is TicketListState.Loading)
+        job.cancel()
+        assertTrue(states[1] is TicketListState.Empty)
+    }
+
+    @Test
+    fun `should return flow of state with loading and error when fetch is failed`() = runTest {
+        val expectedErrorMessage = "Firebase Error"
+        val expectedFlowCount = 2
+
+        //stubbing
+        `when`(getTicketList.execute()).thenThrow(RuntimeException(expectedErrorMessage))
+
+        //execution
+        val states = mutableListOf<TicketListState>()
+        val job = launch {
+            viewModel.ticketListState.toList(states)
+        }
+        viewModel.fetchTicketList()
+        advanceUntilIdle()
+
+        //assertion
+        assertEquals(expectedFlowCount, states.size)
+        assertTrue(states[0] is TicketListState.Loading)
+        job.cancel()
+        assertTrue(states[1] is TicketListState.Error)
+        if(states[1] is TicketListState.Error){
+            val errorState = states[1] as TicketListState.Error
+            assertEquals(expectedErrorMessage, errorState.errorMessage)
         }
     }
 
